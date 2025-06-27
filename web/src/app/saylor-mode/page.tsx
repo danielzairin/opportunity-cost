0856041162c6bcf1134a4699b59cf1768773fd3d
Moses Finlay <islandmoses@gmail.com>
2025-06-27 13:22:08 -0700

    update saylor image

--- web/src/app/saylor-mode/page.tsx
+++ web/src/app/saylor-mode/page.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
       {
         url: "/images/enhanced_saylor.png",
         width: 1200,
-        height: 630,
+        height: 1200,
         alt: "Saylor Mode - Michael Saylor's Bitcoin Prediction",
       },
     ],
